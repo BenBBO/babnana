39da5b4a36629e43691184c6c5328cc42d5f604f
Benjamin Bouillet <YC4116@engie.com>
2020-08-11 21:58:33 +0200

    Add jumbotron + config bootstrap & saas

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 // vue.config.js
 module.exports = {
-    publicPath: './'
-  }
+    publicPath: './',   
+    css: {
+        loaderOptions: {
+          sass: {
+            prependData: "@import '~/node_modules/bootstrap/scss/_functions.scss'; @import '~/node_modules/bootstrap/scss/_variables.scss'; @import '~/node_modules/bootstrap/scss/_mixins.scss'; @import '@/assets/styles/custom.scss';"
+          }
+        }
+      }
+}
