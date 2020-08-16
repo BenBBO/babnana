336cc8c93d38b55db0475a0ef5e8fc73b759d062
Benjamin Bouillet <YC4116@engie.com>
2020-08-16 15:09:34 +0200

    Component for service card

--- src/components/ServicesResume.vue
+++ src/components/ServicesResume.vue
@@ -1,51 +1,44 @@
 <template>
   <div class="card-deck" id="services-container">
-    <div class="card border-0">
-      <img class="card-img-top img-fluid" src="@/assets/birthday.jpg" alt="Card image cap" />
-      <div class="card-body">
-        <h4 class="card-title">Anniversaires</h4>
-        <p
-          class="card-text"
-        >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-    <div class="card border-0">
-      <img class="card-img-top img-fluid" src="@/assets/deguisement.jpg" alt="Card image cap" />
-      <div class="card-body">
-        <h4 class="card-title">Animations</h4>
-        <p
-          class="card-text"
-        >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-    <div class="card border-0">
-      <img class="card-img-top img-fluid" src="@/assets/babysit.jpg" alt="Card image cap" />
-      <div class="card-body">
-        <h4 class="card-title">Garde d'enfants</h4>
-        <p
-          class="card-text"
-        >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
+    <ServiceResumeCard
+      pictureName="birthday.jpg"
+      cardTitle="Anniversaires"
+      cardDetail="Some quick example text to build on the card title and make up the bulk of the card's content."
+      linkTarget="/about"
+      linkText="Go somewhere"
+    />
+    <ServiceResumeCard
+      pictureName="deguisement.jpg"
+      cardTitle="Animations"
+      cardDetail="Some quick example text to build on the card title and make up the bulk of the card's content."
+      linkTarget="/about"
+      linkText="Go somewhere"
+    />
+    <ServiceResumeCard
+      pictureName="babysit.jpg"
+      cardTitle="Garde d'enfants"
+      cardDetail="Some quick example text to build on the card title and make up the bulk of the card's content."
+      linkTarget="/about"
+      linkText="Go somewhere"
+    />
   </div>
 </template>
 
 <script>
+import ServiceResumeCard from "@/components/ServiceResumeCard.vue";
 export default {
-  name: "ServicesResume"
+  name: "ServicesResume",
+  components: { ServiceResumeCard }
 };
 </script>
 
 <style lang="scss" scoped>
-
 #services-container {
   background-color: $primary;
   padding: 30px 15px;
 
   .card-body {
     text-align: center;
-    
   }
 }
 </style>
